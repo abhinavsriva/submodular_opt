8e1103e62d4e33a3fe3bf77a6d1d7d40fc3aeb61
abhinavsriva <45575991+abhinavsriva@users.noreply.github.com>
2020-04-29 14:56:05 +0200

    Add files via upload

---
+++ src/our_proj_grad.m
@@ -0,0 +1,51 @@
+function  [opt_x, opt_f, fs,  runtime] ...
+    = our_proj_grad(f, grad, param, max_iter);
+% fs:  function value in each iteration
+% stepsize: 1/(k +1) 
+fs = [];
+
+n = param.n;
+%
+%
+% start x_0 = max_{x \in P} ||x||
+x1 = zeros(n, 1);
+x = proj_polytope(x1, param);
+%
+%
+%x = zeros(n, 1);
+f_t = f(x, param);
+fs = [fs f_t];
+tic;
+k=0;
+% grad_t = zeros(n,1);
+while k <= max_iter
+    % calculate gradient
+    grad_t = grad(x, param); 
+    gamma = 1/sqrt(k+1);
+    x = x + gamma*grad_t; 
+    % projection to poly 
+    x = proj_polytope(x, param);
+    k = k+1;
+    f_t = f(x, param);
+    fs = [fs f_t];
+end
+runtime = toc; 
+opt_x = x; % possibly the max one 
+opt_f = fs(end);
+end
+
+
+function y  = proj_polytope(x, param);
+
+lb=param.lb;
+ub=param.ub;
+A = param.A;
+b = param.b;
+n = length(ub);
+
+% formulate as QP 
+H = eye(n);
+h = -x;
+opt_quad = optimoptions('quadprog','Display', 'off');
+y = quadprog(H, h, A,b, [], [], lb, ub, [], opt_quad);
+end
